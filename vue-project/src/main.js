369cfb74548038da327b3f4b9dfb85512ea27cfc
Rylie-Tengco <jbtengco@student.apc.edu.ph>
2025-01-27 14:06:55 +0800

    updated

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue' 
-import FoodItem2 from './components/FoodItem2.vue' 
-import PersonalProfile from './components/PersonalProfile.vue' 
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 
 const app = createApp(App)
 
