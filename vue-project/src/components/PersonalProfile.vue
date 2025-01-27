ec3b8cb1f98e01e2c646ea9e92a6586fba9a8ecc
Rylie-Tengco <jbtengco@student.apc.edu.ph>
2025-01-27 13:55:34 +0800

    updated

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,49 +1,54 @@
 <template>
 <h1>Personal Profile Webpage</h1>
 <hr>
- <div class="grid-container">
-        <div class="item1">
-            <h2>About Me</h2>
-            <p>Name: Julian Rylie B. Tengco</p>
-            <p>Nickname: Rylie</p>
-        </div>
-        
-        <div class="item2">
-            <h3>Education / Achievements</h3>
-            <h5>Maranatha Christian Academy of Imus</h5>
-            <ul>
-                <li>Junior High - Senior High (STEM strand)</li>
-                <li>Graduated Senior High with High Honors</li>
-            </ul>
-            <h5>Asia Pacific College (2023-PRESENT)</h5>
-            <ul>
-                <li>BSIT-MI</li>
-                <li>1st Year of College - Honors</li>
-            </ul>
-        </div>
-        
-        <div class="item3">
-            <h3>Course</h3>
-            <p>Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies</p>
-            
-            <h3>IT Experience</h3>
-            <ul>
-                <li>Basics of Java Codes</li>
-                <li>Basics of Python Codes</li>
-            </ul>
-            
-            <h3>Hobbies & Interests</h3>
-            <ul>
-                <li>Gym</li>
-                <li>Volleyball</li>
-                <li>Playing Games</li>
-                <li>Reading Manga and Manhwa</li>
-                <li>Anime</li>
-            </ul>
-        </div>
-        
-        </div>
-    </div>
+<h2>About Me</h2>
+<th><img src="https://scontent.fmnl30-3.fna.fbcdn.net/v/t39.30808-6/397276092_1026365705364210_7686919848526335867_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGEgfwOQ0fRKe74W9gvttiISbU6V23Yf_tJtTpXbdh_-z9zEtt1UxDzDi6-Dh9YaXfEGDdrZG35Tr-9mSiZNxzu&_nc_ohc=khSgy72F12MQ7kNvgH9Z8P4&_nc_zt=23&_nc_ht=scontent.fmnl30-3.fna&_nc_gid=AS53nRW1-M4uyvsSSyZgmg4&oh=00_AYCHjByqmFpOynKAY0e_OptEclsp-RDcamexyS-UKjIC5Q&oe=674D11C9" width="200" height="200"></th>
+<p>Name: Julian Rylie B. Tengco</p>
+ 
+<p>Nickname: Rylie</p>
+ 
+<h3> Education / Achievements </h3>
+<p> 
+<h5>Maranatha Christian Academy of Imus </h5>
+<ul>
+<li>Junior High - Senior High (STEM strand)</li>
+<li>Graduated Senior High with High Honors</li>
+</ul>  
+</p>
+
+<p> <h5>Asia Pacific College (2023-PRESENT)</h5>
+<ul>
+<li>BSIT-MI</li>
+<li>1st Year of College - Honors</li>
+</ul>  
+</p>
+
+<h3> Course </h3>
+<p> Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies </p>
+
+<h3> IT Experience </h3>
+<p> 
+<ul>
+<li>Basics of Java Codes</li>
+<li>Basics of Python Codes</li>
+</ul>  
+</p>
+
+<h3> Hobbies & Interests </h3>
+<ul>
+<li>Gym</li>
+<li>Volleyball</li>
+<li>Playing Games</li>
+<li>Reading Manga and Manhwa</li>
+<li>Anime</li>
+</ul>  
+
+<h3> Goals In Life / Dream </h3>
+<ul>
+<li>Be Successful</li>
+<li>Have a Stable Income</li>
+<li>Live a Happy Life</li>
+</ul>
 </template>
 
 <script></script>
