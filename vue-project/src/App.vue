cbdc182f04c390d721eb48d0cfaa0379aad9ed5e
Rylie-Tengco <jbtengco@student.apc.edu.ph>
2025-01-27 14:02:01 +0800

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>Food</h1>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <personl-profile/> <br>
+  <food-item/>
+  <food-item2/>
+  <food-item/>
+  <personl-profile/>
 </template>
 
 <script></script>
