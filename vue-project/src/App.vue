ad4557a2a6ae0cf4fea242b504e90053b77ecd24
Rylie-Tengco <jbtengco@student.apc.edu.ph>
2025-01-27 13:41:26 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,13 +6,3 @@
 </template>
 
 <script></script>
-
-<style>
-  #app > div {
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightgreen;
-    }
-</style> 
