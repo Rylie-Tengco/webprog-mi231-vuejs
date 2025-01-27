9237413a4578547d7335e7dd6b248353a7915ab3
Rylie-Tengco <jbtengco@student.apc.edu.ph>
2025-01-27 13:53:43 +0800

    Added personal profile

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,50 @@
+<template>
+<h1>Personal Profile Webpage</h1>
+<hr>
+ <div class="grid-container">
+        <div class="item1">
+            <h2>About Me</h2>
+            <p>Name: Julian Rylie B. Tengco</p>
+            <p>Nickname: Rylie</p>
+        </div>
+        
+        <div class="item2">
+            <h3>Education / Achievements</h3>
+            <h5>Maranatha Christian Academy of Imus</h5>
+            <ul>
+                <li>Junior High - Senior High (STEM strand)</li>
+                <li>Graduated Senior High with High Honors</li>
+            </ul>
+            <h5>Asia Pacific College (2023-PRESENT)</h5>
+            <ul>
+                <li>BSIT-MI</li>
+                <li>1st Year of College - Honors</li>
+            </ul>
+        </div>
+        
+        <div class="item3">
+            <h3>Course</h3>
+            <p>Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies</p>
+            
+            <h3>IT Experience</h3>
+            <ul>
+                <li>Basics of Java Codes</li>
+                <li>Basics of Python Codes</li>
+            </ul>
+            
+            <h3>Hobbies & Interests</h3>
+            <ul>
+                <li>Gym</li>
+                <li>Volleyball</li>
+                <li>Playing Games</li>
+                <li>Reading Manga and Manhwa</li>
+                <li>Anime</li>
+            </ul>
+        </div>
+        
+        </div>
+    </div>
+</template>
+
+<script></script>
+<style></style>
