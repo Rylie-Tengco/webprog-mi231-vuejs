02a3dc9228931c68e3e1ee831e14c25d84d5d1d6
Rylie-Tengco <jbtengco@student.apc.edu.ph>
2025-01-27 14:08:18 +0800

    updated

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,21 +1,47 @@
 <template>
-<div class="container">
-    <header> <h1>Personal Profile Web page </h1>  </header>
-    <main>
-      <section class="main-content">
-        <div>1 About Me</div>
-        <div>2 Education / Achievements</div>
-        <div>3 Course </div>  
-        <div>4 IT Experience</div>
-        <div>5 Hobbies & Interests</div>
-        <div>6 Goals In Life / Dream</div>  
-        <div>7 Picture Gallery</div>
-      </section>
-      <aside class="sidebar">
-        </aside>
-    </main>
-    <footer> Asia Pacific College   </footer>
-  </div>
+<div class="grid-container">
+        <div class="item1">
+            <h2>About Me</h2>
+            <p>Name: Julian Rylie B. Tengco</p>
+            <p>Nickname: Rylie</p>
+        </div>
+        
+        <div class="item2">
+            <h3>Education / Achievements</h3>
+            <h5>Maranatha Christian Academy of Imus</h5>
+            <ul>
+                <li>Junior High - Senior High (STEM strand)</li>
+                <li>Graduated Senior High with High Honors</li>
+            </ul>
+            <h5>Asia Pacific College (2023-PRESENT)</h5>
+            <ul>
+                <li>BSIT-MI</li>
+                <li>1st Year of College - Honors</li>
+            </ul>
+        </div>
+        
+        <div class="item3">
+            <h3>Course</h3>
+            <p>Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies</p>
+            
+            <h3>IT Experience</h3>
+            <ul>
+                <li>Basics of Java Codes</li>
+                <li>Basics of Python Codes</li>
+            </ul>
+            
+            <h3>Hobbies & Interests</h3>
+            <ul>
+                <li>Gym</li>
+                <li>Volleyball</li>
+                <li>Playing Games</li>
+                <li>Reading Manga and Manhwa</li>
+                <li>Anime</li>
+            </ul>
+        </div>
+        
+        </div>
+    </div>
 </template>
 
 <script></script>
